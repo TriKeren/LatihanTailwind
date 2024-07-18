fc2fd3d2997fcb98728240f76443e5f69f83fbdc
damskuyyy <muqoddam2711@gmail.com>
2024-07-18 15:53:27 +0700

    push lagi

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Route,Routes,BrowserRouter } from 'react-router-dom'
 import Home from './page/Home'
+import Inwood from './page/Inwood'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/inwood-ecommerce-landingpage' element={<Inwood/>}/>
       </Routes>
     </BrowserRouter>
   )
