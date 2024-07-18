dbcb4129799e0ac667b05fa68c5ea4bd6074cb56
Nafiskurniatul <92704434+Nafiskurniatul@users.noreply.github.com>
2024-07-18 21:19:09 +0700

    Assets (Foto), navbar, components (fordentist)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Route,Routes,BrowserRouter } from 'react-router-dom'
-import Home from './page/Home'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from "./page/Home";
+import Fordentist from "./page/Fordentist";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="landingpage-for-dentist" element={<Fordentist />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
