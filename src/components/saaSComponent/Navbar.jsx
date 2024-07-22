c3a39abb72470bdb9217e673bd1436615b4ae2da
wildandaffa15 <161112149+wildandaffa15@users.noreply.github.com>
2024-07-22 16:02:24 +0700

    upload-npm-Saas

--- src/components/saaSComponent/Navbar.jsx
+++ src/components/saaSComponent/Navbar.jsx
@@ -22,34 +22,7 @@ const Navbar = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-
-    return (
-        <nav className={`bg-[#043873] py-4 lg:py-4 md:py-4 sticky top-0 z-50 transition-all duration-300 ${scrolled ? 'bg-opacity-80 backdrop-blur-md' : 'bg-opacity-500'}`}>
-            <div className="container mx-auto flex justify-between items-center max-w-screen-xl px-4 md:px-8">
-                <div className="flex items-center">
-                    <img src="public/assets/saaS-landingpages/Logo.svg" className="h-10 w-[133.97px] lg:w-2/4 md:w-2/3" />
-                </div>
-                <div className="hidden lg:flex space-x-6 items-center gap-10 font-[DM Sans] text-white font-medium">
-                    <ul className="flex space-x-6 gap-8">
-                        <li><a href="" className="hover:text-[#4F9CF9]">Product</a></li>
-                        <li><a href="" className="hover:text-[#4F9CF9]">Solution</a></li>
-                        <li><a href="" className="hover:text-[#4F9CF9]">Resource</a></li>
-                        <li><a href="" className="hover:text-[#4F9CF9]">Pricing</a></li>
-                    </ul>
-                    <div className="space-x-4">
-                        <button className="px-4 py-2 bg-[#FFE492] text-[#043873] rounded hover:bg-[#4F9CF9] hover:text-[#FFE492]">Login</button>
-                        <button className="px-4 py-2 bg-[#4F9CF9] text-[#FFE492] rounded hover:bg-[#FFE492] hover:text-[#043873]">Try Whitepace free</button>
-                    </div>
-                </div>
-                <div className="lg:hidden">
-                    <button onClick={toggleMobile} className="text-white focus:outline-none">
-                        <svg className="w-[42px] h-auto" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-                        </svg>
-                    </button>
-                </div>
-            </div>
-=======
+  
   return (
     <nav
       className={`bg-[#043873] shadow-md py-4 sticky top-0 z-50 transition-all duration-300 ${
